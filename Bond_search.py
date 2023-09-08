f56714e47d671ed8377f5868dd86eee3461f95b2
ReinhardLenz <71219487+ReinhardLenz@users.noreply.github.com>
2023-09-08 09:24:00 +0300

    Add files via upload
    seperation of driver connect and web procedures file to sub modules, imported in main program

---
+++ Bond_search.py
@@ -0,0 +1,133 @@
+import config
+import web_actions
+import os
+#import msvcrt
+import time
+import csv
+import numpy as np
+import pandas as pd 
+import random
+import xlsxwriter
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from POMProjectFolder.Stuttgart.Pages.locators import Locators
+from datetime import date
+
+config.driver.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+params = {'cmd': 'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': "/path/to/download/dir"}}
+command_result = config.driver.execute("send_command", params)
+
+web_actions.zoom40()
+config.driver.get("https://www.boerse-stuttgart.de/de-de/tools/produktsuche/anleihen-finder/")
+#Click Cookie acceptor 
+web_actions.click_operation_B_Xpath(Locators.cookie_acceptor_full_XP)
+#Make "tradable unit" button visible
+config.driver.execute_script ("window.scrollTo(0,document.body.scrollHeight);")
+
+print("Click on Zusaetzliche Filter")
+config.driver.execute_script("document.body.style.zoom='40%'")# 
+web_actions.click_operation_css(Locators.Zusatzliche_Filter_css)
+web_actions.relax()
+print("Click on checkbox for handelbare einheit")
+web_actions.write_operation_b_xpath(Locators.Zusatzliche_Filter_Textfield_Full_XPath,"Handelbare Einheit")
+#Click "Bond tradable unit"
+web_actions.click_operation_id_A(Locators.Handelbare_einh_Button_id)
+config.driver.execute_script("document.body.style.zoom='40%'")# 
+web_actions.click_operation_Xpath(Locators.Zusatzliche_Filter_Anwenden_Full_XPath)
+web_actions.click_operation_Xpath(Locators.Handelbare_einh_Button_xp)
+web_actions.clear_operation_A_xpath(Locators.min_Littera_xp)
+web_actions.write_operation_b_xpath(Locators.min_Littera_xp,"1")
+time.sleep(random.randint(2,6))
+web_actions.clear_operation_A_xpath(Locators.max_Littera_xp)
+web_actions.write_operation_b_xpath(Locators.max_Littera_xp,"50.000,000")
+web_actions.click_operation_Xpath(Locators.Anwenden_littera_full_XPath)
+# Bond Due date Between two dates
+web_actions.click_operation_Xpath(Locators.Falligkeit_click_xp)
+time.sleep(random.randint(2,6))
+web_actions.clear_operation_A_xpath(Locators.Smaller_date_xp)
+web_actions.write_operation_xpath(Locators.Smaller_date_xp,"2.2.2027")
+time.sleep(random.randint(2,6))
+web_actions.click_operation_Xpath(Locators.Bigger_date_xp)
+web_actions.clear_operation_A_xpath(Locators.Bigger_date_2_xp)
+web_actions.write_operation_xpath(Locators.Bigger_date_2_xp,"1.4.2035")
+web_actions.click_operation_Xpath(Locators.Anwenden_Falligkeit_full_XPath)
+config.driver.implicitly_wait(10)
+#Gelisteter Zeitraum Bond Listed date from to date 
+web_actions.click_operation_css(Locators.Zusatzliche_Filter_css)
+web_actions.relax()
+web_actions.write_operation_b_xpath(Locators.Zusatzliche_Filter_Textfield_Full_XPath,"Gelist")
+time.sleep(random.randint(2,6))
+web_actions.click_operation_id_A(Locators.Gelisteter_Zeitraum_preselector_Button_id)
+web_actions.click_operation_Xpath(Locators.Zusatzliche_Filter_Anwenden_Full_XPath)
+web_actions.click_operation_Xpath(Locators.Gelisteter_Zeitraum_Button_xp)
+time.sleep(random.randint(2,6))
+web_actions.clear_operation_A_xpath(Locators.Gelisteter_Zeitraum_smaller_date_xp)
+web_actions.write_operation_xpath(Locators.Gelisteter_Zeitraum_smaller_date_xp,"1.1.2020")
+web_actions.click_operation_Xpath(Locators.Gelisteter_Zeitraum_Bigger_date_xp)
+time.sleep(random.randint(2,6))
+web_actions.clear_operation_A_xpath(Locators.Gelisteter_Zeitraum_Bigger_date_xp)
+web_actions.write_operation_xpath(Locators.Gelisteter_Zeitraum_Bigger_date_xp,"1.8.2023")
+time.sleep(random.randint(2,6))
+web_actions.click_operation_Xpath(Locators.Anwenden_Gelisteter_Zeitraum_full_XPath)
+#Select Unternehmensanleihe = Bond
+web_actions.click_operation_id_A(Locators.Preselector_Anleihen_Typ_ID)
+web_actions.click_operation_id_A(Locators.Select_Unternehmensanleihe_Checkbox_ID)
+web_actions.click_operation_Xpath(Locators.Anwenden_Unternehmensanleihe_Select_full_XPath)
+# Choose Yield between maximum and minimum
+web_actions.click_operation_Xpath(Locators.Rendite_button_xp)
+web_actions.clear_operation_A_xpath(Locators.Rendite_lower_margin_xp)
+web_actions.write_operation_xpath(Locators.Rendite_lower_margin_xp, "3%")
+web_actions.clear_operation_A_xpath(Locators.Rendite_upper_margin_xp)
+web_actions.write_operation_xpath(Locators.Rendite_upper_margin_xp, "16%")
+time.sleep(random.randint(2,6))
+web_actions.click_operation_Xpath(Locators.Rendite_anwenden_xp)
+#Choosing the acceptable currency Euro
+web_actions.click_operation_Xpath(Locators.Wahrung_click_xp)
+time.sleep(random.randint(2,6))
+web_actions.click_operation_id_A(Locators.Wahrung_click_new_ID)
+print("Click Anwenden of euro ")
+time.sleep(random.randint(2,6))
+web_actions.click_operation_css(Locators.Wahrung_anwenden_css)
+#Sort by yield
+time.sleep(random.randint(2,6))
+config.driver.execute_script ("window.scrollTo(0,document.body.scrollHeight);")
+#show_more_hits
+web_actions.click_operation_Xpath(Locators.Weitere_Treffer_anzeigen_xp)
+_j=1
+_n=5
+while _j<_n:
+    config.driver.execute_script ("window.scrollTo(0,document.body.scrollHeight);")
+    time.sleep(random.randint(2,6))
+    web_actions.click_operation_Xpath(Locators.Weitere_Treffer_anzeigen_xp)
+    _j+=1
+    time.sleep(random.randint(2,6))
+
+# Write Results to Excel file
+html_source =config.driver.page_source
+soup=BeautifulSoup(html_source, 'html.parser')
+table=soup.table
+table_rows = table.find_all('tr')
+tables=[]
+for tr in table_rows:
+    td = tr.find_all('td')
+    row=[]
+    for i in td:
+        word= i.text.strip()
+        word=word.replace(".000", "000") #littera remove seperators
+        word=word.replace(",000", "") #littera remove zeros after comma
+        row.append(word)
+    tables.append(row)
+print(tables)
+df = pd.DataFrame(tables)
+#df.to_excel(excel_writer = "/var/www/10/raikkulenz/sites/raikkulenz.kapsi.fi/www/python/anleihen/test.xlsx")
+df.to_excel(excel_writer = "c:\\temp\\Stuttgart_test.xlsx")
+
+config.driver.quit()
+
+a="konifer"
+web_actions.do_something(a)
+web_actions.do_something_else()
