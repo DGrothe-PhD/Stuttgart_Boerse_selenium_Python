1fb3c1c59276efa25a62bd61cece7ecfabd46878
Reinhard Lenz <71219487+ReinhardLenz@users.noreply.github.com>
2024-01-31 13:18:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-chromedriver_location = "C:\\Users\\lenzre\\Documents\\programs\\chromedriver.exe"
-service = Service(executable_path = chromedriver_location)
+#from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+
 chrome_options = Options()
 chrome_options.add_experimental_option("prefs", {
   "download.default_directory": "/path/to/download/dir",
@@ -14,5 +16,10 @@ chrome_options.add_argument("--disable-gpu")#OLDER OPTIONS
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])#OLDER OPTIONS
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--remote-debugging-port=9222")
-driver = webdriver.Chrome(service = service, options=chrome_options)
+chrome_options.add_argument("--disable-infobars")
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_experimental_option(
+    "prefs", {"profile.default_content_setting_values.notifications": 1})
 
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
